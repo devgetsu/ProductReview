ad91935d8177a96d7acab41893d013ef9a76964e
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-03-02 23:52:07 +0500

    repository added

---
+++ ProductReview.Infrastruct/Repositories/ProductRepository.cs
@@ -0,0 +1,20 @@
+﻿using ProductReview.Application.Astractions.RepositoryInterfaces;
+using ProductReview.Domain.Entities.Models;
+using ProductReview.Infrastruct.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReview.Infrastruct.Repositories
+{
+    public class ProductRepository : BaseRepository<Product>, IProductRepository
+    {
+        public ProductRepository(AppDbContext db)
+            : base(db)
+        {
+
+        }
+    }
+}
---
+++ ProductReview.Infrastruct/Repositories/UserRepository.cs
@@ -0,0 +1,18 @@
+﻿using ProductReview.Application.Astractions.RepositoryInterfaces;
+using ProductReview.Domain.Entities.Models;
+using ProductReview.Infrastruct.Persistance;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReview.Infrastruct.Repositories
+{
+    public class UserRepository : BaseRepository<User>, IUserRepository
+    {
+        public UserRepository(AppDbContext context) : base(context)
+        {
+        }
+    }
+}
