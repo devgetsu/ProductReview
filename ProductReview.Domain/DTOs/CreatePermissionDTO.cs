ad2ba4e94b70e9bebe99919f066ba787daf6bf1a
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-03-03 00:06:37 +0500

    DTOs Created

---
+++ ProductReview.Domain/DTOs/CreatePermissionDTO.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ProductReview.Domain.DTOs
+{
+    public class CreatePermissionDTO
+    {
+        public string Name { get; set; }
+    }
+}
